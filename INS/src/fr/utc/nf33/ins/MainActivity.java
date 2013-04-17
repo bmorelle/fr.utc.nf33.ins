a1046b4d67e0c39faca5c86063b2d801ae5ef779
Clément Contini <vilisseranen@gmail.com>
2013-04-17 15:36:05 +0200

    Creation et configuration de la carte

--- INS/src/fr/utc/nf33/ins/MainActivity.java
+++ INS/src/fr/utc/nf33/ins/MainActivity.java
@@ -1,24 +1,40 @@
 package fr.utc.nf33.ins;
 
-import fr.utc.nf33.ins.R;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.SupportMapFragment;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends FragmentActivity {
 
+	private SupportMapFragment mapFragment;
+	private GoogleMap map;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	private static GoogleMapOptions GOOGLE_MAP_OPTIONS = (new GoogleMapOptions())
+			.compassEnabled(false).mapType(GoogleMap.MAP_TYPE_NORMAL)
+			.rotateGesturesEnabled(true).tiltGesturesEnabled(true)
+			.zoomControlsEnabled(false).zoomGesturesEnabled(true);
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mapFragment = SupportMapFragment.newInstance(GOOGLE_MAP_OPTIONS);
+		FragmentTransaction fragmentTransaction = getSupportFragmentManager()
+				.beginTransaction();
+		fragmentTransaction.add(R.id.map_fragment_container, mapFragment);
+		fragmentTransaction.commit();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
