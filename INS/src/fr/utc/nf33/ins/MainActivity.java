de1755922887e053e8cbd637e57c12c195729c93
Clément Contini <vilisseranen@gmail.com>
2013-04-17 16:07:28 +0200

    Ajout d'un listener sur le location manager

--- INS/src/fr/utc/nf33/ins/MainActivity.java
+++ INS/src/fr/utc/nf33/ins/MainActivity.java
@@ -1,9 +1,15 @@
 package fr.utc.nf33.ins;
 
+import android.content.Context;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
@@ -12,7 +18,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 public class MainActivity extends FragmentActivity {
 
 	private SupportMapFragment mapFragment;
-	private GoogleMap map;
+	private LocationManager locationManager;
+	private LocationProvider provider;
 
 	private static GoogleMapOptions GOOGLE_MAP_OPTIONS = (new GoogleMapOptions())
 			.compassEnabled(false).mapType(GoogleMap.MAP_TYPE_NORMAL)
@@ -29,6 +36,20 @@ public class MainActivity extends FragmentActivity {
 				.beginTransaction();
 		fragmentTransaction.add(R.id.map_fragment_container, mapFragment);
 		fragmentTransaction.commit();
+		
+		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		provider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+		locationManager.addGpsStatusListener(new GpsStatus.Listener() {
+			@Override
+			public void onGpsStatusChanged(int event) {
+				GpsStatus status = locationManager.getGpsStatus(null);
+				float avg = 0;
+				for (GpsSatellite sat : status.getSatellites())
+					avg += sat.getSnr();
+				avg /= status.getMaxSatellites();
+				((TextView) MainActivity.this.findViewById(R.id.toto)).setText(Float.toString(avg));
+			}
+		});
 	}
 
 	@Override
