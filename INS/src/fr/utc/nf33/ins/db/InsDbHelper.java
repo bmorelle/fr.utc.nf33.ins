bd37cc053009d70c3a7af3426539a00a21f146e0
Baptiste Morelle <brm.morelle@gmail.com>
2013-05-08 17:33:47 +0200

    created the database, imported the SQLiteAssetHelper jar and implemented the InsDbHelper

---
+++ INS/src/fr/utc/nf33/ins/db/InsDbHelper.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package fr.utc.nf33.ins.db;
+
+import android.content.Context;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+/**
+ * 
+ * @author
+ * 
+ */
+public class InsDbHelper extends SQLiteAssetHelper {
+  //
+  private static final String DATABASE_NAME = "ins";
+  //
+  private static final int DATABASE_VERSION = 1;
+
+  /**
+   * 
+   * @param context
+   */
+  public InsDbHelper(Context context) {
+    super(context, DATABASE_NAME, null, DATABASE_VERSION);
+  }
+}
---
+++ INS/src/fr/utc/nf33/ins/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ * @author
+ * 
+ */
+package fr.utc.nf33.ins;
