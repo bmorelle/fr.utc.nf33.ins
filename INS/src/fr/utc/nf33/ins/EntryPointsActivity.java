281b9ac17e24ed24c317416da57621cdba8c56b6
Baptiste Morelle <brm.morelle@gmail.com>
2013-05-15 15:06:43 +0200

    the Entry Points database query now works

--- INS/src/fr/utc/nf33/ins/EntryPointsActivity.java
+++ INS/src/fr/utc/nf33/ins/EntryPointsActivity.java
@@ -6,12 +6,9 @@ package fr.utc.nf33.ins;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
-import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
-import fr.utc.nf33.ins.db.DbCursorLoader;
 import fr.utc.nf33.ins.db.InsContract;
 import fr.utc.nf33.ins.db.InsDbHelper;
 
@@ -20,11 +17,9 @@ import fr.utc.nf33.ins.db.InsDbHelper;
  * @author
  * 
  */
-public class EntryPointsActivity extends FragmentActivity
-    implements
-      LoaderManager.LoaderCallbacks<Cursor> {
-  // This is the Adapter being used to display the list's data.
-  private CursorAdapter cursorAdapter;
+public class EntryPointsActivity extends FragmentActivity {
+  //
+  private Cursor cursor;
 
   //
   private InsDbHelper dbHelper;
@@ -38,56 +33,28 @@ public class EntryPointsActivity extends FragmentActivity
     setContentView(R.layout.activity_entry_points);
 
     //
-    dbHelper = new InsDbHelper(this);
     listView = (ListView) findViewById(R.id.entry_points_list);
+    dbHelper = new InsDbHelper(this);
+    cursor =
+        dbHelper.getReadableDatabase().rawQuery("SELECT idBuilding AS _id, name FROM Building",
+            null);
 
     // For the cursor adapter, specify which columns go into which views.
     String[] fromColumns = {InsContract.Building.COLUMN_NAME_NAME};
     int[] toViews = {R.id.entry_points_list_item_text};
 
     // Create an empty adapter we will use to display the loaded data.
-    // We pass null for the cursor, then update it in onLoadFinished().
-    cursorAdapter =
-        new SimpleCursorAdapter(this, R.layout.entry_points_list_item, null, fromColumns, toViews,
-            0);
+    ListAdapter cursorAdapter =
+        new SimpleCursorAdapter(this, R.layout.entry_points_list_item, cursor, fromColumns,
+            toViews, 0);
     listView.setAdapter(cursorAdapter);
-
-    // Prepare the loader. Either re-connect with an existing one, or start a new one.
-    getSupportLoaderManager().initLoader(0, null, this);
   }
 
   @Override
   protected void onDestroy() {
     super.onDestroy();
 
+    cursor.close();
     dbHelper.close();
-  }
-
-  // Called when a new Loader needs to be created.
-  @Override
-  public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-    return new DbCursorLoader(this) {
-      @Override
-      public Cursor getCursor() {
-        return dbHelper.getReadableDatabase().rawQuery("SELECT * FROM Building", null);
-      }
-    };
-  }
-
-  // Called when a previously created loader is reset, making the data unavailable.
-  @Override
-  public void onLoaderReset(Loader<Cursor> loader) {
-    // This is called when the last Cursor provided to onLoadFinished()
-    // above is about to be closed. We need to make sure we are no
-    // longer using it.
-    cursorAdapter.swapCursor(null);
-  }
-
-  // Called when a previously created loader has finished loading.
-  @Override
-  public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-    // Swap the new cursor in. (The framework will take care of closing the
-    // old cursor once we return.)
-    cursorAdapter.swapCursor(data);
   }
 }
